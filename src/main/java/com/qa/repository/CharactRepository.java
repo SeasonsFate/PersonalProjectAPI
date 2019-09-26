5a133a17ab3166f274e76250c2ae3faacbe6c071
SeasonsFate <alexbrew6@gmail.com>
2019-09-27 00:01:46 +0100

    fixed some more codacy issues

--- src/main/java/com/qa/repository/CharactRepository.java
+++ src/main/java/com/qa/repository/CharactRepository.java
@@ -1,7 +1,6 @@
 package com.qa.repository;
 
 import com.qa.models.Charact;
-import com.qa.models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CharactRepository extends JpaRepository<Charact, Long>{
--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class AppTest {
 
 
-    App app;
+    private App app;
 
     @Test
     public void shouldAnswerWithTrue() {
